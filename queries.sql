f98ad7744a4ae43d42abd7a8b987ce88b9c53275
Duka <duka.fortin251@gmail.com>
2026-08-19 12:22:05 +0200

    final updateee

--- queries.sql
+++ queries.sql
@@ -1,1 +1,19 @@
+select * FROM objet  
 
+-- Quels objets sont actuellement en rayon, et depuis combien de temps ?
+
+-- Quelle catégorie se vend le mieux ? Laquelle rapporte le plus ?
+
+-- Combien d'heures de bénévolat ont été consacrées à la réparation cette année ?
+
+-- Quel est le taux de réussite des réparations, par bénévole et globalement ?
+
+-- Quelles personnes nous ont fait plus de trois dépôts ?
+
+-- Quel poids total avons-nous détourné de la déchetterie (tout ce qui n'est pas recyclé) ?
+
+-- Quel est le taux de présence réelle sur nos ateliers ?
+
+-- Quels bénévoles ont la compétence « électricité » et sont disponibles pour animer un atelier ?
+
+-- Quels objets sont en rayon depuis plus de six mois et devraient être sortis ?
